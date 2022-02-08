3414ed6e6dad8a843f49c3200e1e6cc05a4cdb5d
unknown <ymt.yemyattun@gmail.com>
2022-02-08 14:12:01 +0630

    commit 2

--- AutoID.php
+++ AutoID.php
@@ -1,14 +1,29 @@
 <?php 
-	//example -> GeneratedAutoId("M", 5, 12) = M-00012
-	function GeneratedAutoID($prefix, $letterCount, $dataCount)
+	function AutoID($prefix, $characterCount, $tableName, $fieldName)
 	{
-		$dataCountLength = strlen((string)$dataCount);
-		$leadingZeroCount = $letterCount - $dataCountLength;
+		$connect = mysqli_connect('localhost', 'root', '', 'testing');	//connect to database
+		$select = "SELECT $fieldName FROM $tableName ORDER BY $fieldName DESC LIMIT 1";	//select last ID
+		$query = mysqli_query($connect, $select);	//execute sql code
+		$count = mysqli_num_rows($query);	//count the number of rows of the executed sql code
+		$array = mysqli_fetch_array($query);	//fetch the data
+
 		$leadingZeroString = "";
+		$leadingZeroCount = $characterCount - strlen((string)$count);
 		for ($i=0; $i < $leadingZeroCount; $i++) { 
 			$leadingZeroString = $leadingZeroString . "0";
 		}
-		$id = $prefix . "-" . $leadingZeroString . $dataCount;
-		return $id;
+
+		if ($count < 1) {
+			$newID = $prefix . "-" . $leadingZeroString . "1";
+			return $newID; 
+		}
+		else
+		{
+			$lastID = $array[$fieldName];	//lastID
+			$lastID = trim($lastID, $prefix . "-");	//remove prefix and -
+			$newValue = (int)$lastID + 1;	//plus one to previous id
+			$newID = $prefix . "-" . $leadingZeroString . $newValue;
+			return $newID;
+		}
 	}
 ?>
