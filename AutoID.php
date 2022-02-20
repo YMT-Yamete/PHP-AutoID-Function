1717e7bbe49c4d9902bf973ac693089c5d93b116
Ye Myat Tun <87070128+YMT-Yamete@users.noreply.github.com>
2022-02-20 20:25:44 +0630

    Update AutoID.php

--- AutoID.php
+++ AutoID.php
@@ -1,19 +1,19 @@
 <?php 
 	function AutoID($prefix, $characterCount, $tableName, $fieldName)
 	{
-		$connect = mysqli_connect('localhost', 'root', '', 'testing');	//connect to database
+		$connect = mysqli_connect('localhost', 'root', '', 'hexauniversity');	//connect to database
 		$select = "SELECT $fieldName FROM $tableName ORDER BY $fieldName DESC LIMIT 1";	//select last ID
 		$query = mysqli_query($connect, $select);	//execute sql code
 		$count = mysqli_num_rows($query);	//count the number of rows of the executed sql code
 		$array = mysqli_fetch_array($query);	//fetch the data
 
-		$leadingZeroString = "";
-		$leadingZeroCount = $characterCount - strlen((string)$count);
-		for ($i=0; $i < $leadingZeroCount; $i++) { 
-			$leadingZeroString = $leadingZeroString . "0";
-		}
-
 		if ($count < 1) {
+			$leadingZeroString = "";
+			$leadingZeroCount = $characterCount - strlen((string)$count);
+			for ($i=0; $i < $leadingZeroCount; $i++) { 
+				$leadingZeroString = $leadingZeroString . "0";
+			}
+
 			$newID = $prefix . "-" . $leadingZeroString . "1";
 			return $newID; 
 		}
@@ -22,6 +22,13 @@
 			$lastID = $array[$fieldName];	//lastID
 			$lastID = trim($lastID, $prefix . "-");	//remove prefix and -
 			$newValue = (int)$lastID + 1;	//plus one to previous id
+
+			$leadingZeroString = "";
+			$leadingZeroCount = $characterCount - strlen((string)$newValue);
+			for ($i=0; $i < $leadingZeroCount; $i++) { 
+				$leadingZeroString = $leadingZeroString . "0";
+			}
+
 			$newID = $prefix . "-" . $leadingZeroString . $newValue;
 			return $newID;
 		}
